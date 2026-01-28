251762169393b903fb176a660d6a135e8b719a5b
MelissaOneil <melissah29@gmail.com>
2026-01-28 15:38:10 -0700

    feat: enable cookie-based session storage.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-LingoLinq::Application.config.session_store :disabled
+#LingoLinq::Application.config.session_store :disabled
+
+# Use cookies for session storage
+LingoLinq::Application.config.session_store :cookie_store, key: '_lingolinq_session'
