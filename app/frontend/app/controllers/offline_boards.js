692229121650bcbc95f407905300b3bc2d17665f
MelissaOneil <melissah29@gmail.com>
2026-06-04 18:49:41 -0600

    Route Basic Access boards into the demo speak shell, align speech locale/voice with the loaded board, and add demo quick-word support. Also updates demo board sizing, clear icon parity, full-message speech, and cloud voice fallback.

--- app/frontend/app/controllers/offline_boards.js
+++ app/frontend/app/controllers/offline_boards.js
@@ -1,14 +1,12 @@
 import Controller from '@ember/controller';
-import app_state from '../utils/app_state';
+import { inject as service } from '@ember/service';
 import i18n from '../utils/i18n';
-import obf from '../utils/obf';
 import emergency from '../utils/obf-emergency';
-import persistence from '../utils/persistence';
-import LingoLinq from '../app';
-import { later as runLater } from '@ember/runloop';
 import { computed, observer, set as emberSet } from '@ember/object';
 
 export default Controller.extend({
+  router: service('router'),
+
   preferred_locale: computed('last_locale', function() {
     if(this.get('last_locale')) { return this.get('last_locale'); }
     return navigator.language.toLowerCase().split(/-|_/)[0];
@@ -18,6 +16,10 @@ export default Controller.extend({
     var pref = this.get('preferred_locale');
     for(var key in emergency.boards) {
       var starters = (emergency.boards[key] || []).filter(function(b) { return b.starter; });
+      starters.forEach(function(b) {
+        emberSet(b, 'locale', key);
+        emberSet(b, 'path', b.path || ('obf/emergency-' + key + '_' + b.id));
+      });
       var str = i18n.locales_localized[key] || i18n.locales[key] || key;
       var credit = "";
       if(key == 'pl') {
@@ -60,13 +62,19 @@ export default Controller.extend({
     pick: function(board) {
       window.emergency = emergency;
       var list = this.get('locales');
+      var boardKey = board.path;
       list.forEach(function(loc) {
         loc.boards.forEach(function(b) {
           console.log(b.id, board.id);
           emberSet(b, 'chosen', (b == board));
         });  
       });
-      app_state.home_in_speak_mode({reminded: true, force_board_state: {key: board.path}});
+      this.get('router').transitionTo('demo.speak', {
+        queryParams: {
+          board: boardKey,
+          source: 'offline_boards'
+        }
+      });
     }
   }
 });
